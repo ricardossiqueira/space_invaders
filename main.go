675ffdef73f3b75c3f382cd3efc3ee55c440eca8
Ricardo <ricardosantossiqueira@poli.ufrj.br>
2024-04-08 20:46:31 -0300

    testing gameloop

--- main.go
+++ main.go
@@ -6,41 +6,60 @@ import (
 	"time"
 )
 
-func main() {
+type gameLoop struct {
+	delta       time.Duration
+	lastUpdate  time.Time
+	accumulator time.Duration
+	slice       time.Duration
+}
 
+func stress(c *cli.Cli) {
+	for y := 0; y < c.Size.H; y++ {
+		for x := 0; x < c.Size.W; x++ {
+			c.ColorFprintfSprite([]string{"#"}, cli.Colors[rand.Intn(len(cli.Colors))], cli.Coord{X: x, Y: y})
+		}
+	}
+}
+
+func main() {
 	// init
 	c, err := cli.New()
 	if err != nil {
 		panic("cli.New(): Failed to retrieve window dimensions")
 	}
+	gl := &gameLoop{lastUpdate: time.Now(), slice: time.Millisecond * 10} // update ratio
+	ticker := time.NewTicker(time.Millisecond * 150)                      // render ratio
 
-	// update
-	c.HandleSIGTERM(func() { c.MoveCursor(cli.Coord{X: 0, Y: 0}) }, c.ShowCursor)
+	// handle ctrl+c
+	c.HandleSIGTERM(c.ShowCursor)
 
+	// setup
 	c.ClearCli()
 	c.HideCursor()
 
-	c.ColorFprintfSprite([]string{" ⢀⣀⣾⣷⣀⡀ ", " ⣿⣿⣿⣿⣿⣿ "}, cli.Green, cli.Coord{X: 2, Y: 5})
-
-	ticker := time.NewTicker(time.Millisecond * 200)
-
 	for {
+		<-ticker.C
 
-		select {
-		case <-ticker.C:
-			// draw
-			c.ClearCli()
-			c.Render()
+		gl.delta = time.Since(gl.lastUpdate)
+		gl.lastUpdate = time.Now()
 
-			// update
-			// t := time.Now()
-			// c.ColorFprintfSprite([]string{t.Format(time.TimeOnly)}, cli.Green, cli.Coord{X: 1, Y: 1})
-			for y := 0; y < c.Size.H; y++ {
-				for x := 0; x < c.Size.W; x++ {
-					c.ColorFprintfSprite([]string{"#"}, cli.Colors[rand.Intn(len(cli.Colors))], cli.Coord{X: x, Y: y})
-				}
+		// update
+		for {
+			if gl.accumulator > gl.slice {
+				break
 			}
+
+			stress(c)
+			// c.ColorFprintfSprite([]string{" ⢀⣀⣾⣷⣀⡀ ", " ⣿⣿⣿⣿⣿⣿ "}, cli.Green, cli.Coord{X: 2, Y: 5})
+
+			// after update
+			gl.accumulator -= gl.slice
+
 		}
+
+		// draw
+		c.Render()
+		c.ClearCli()
 	}
 
 	// end
